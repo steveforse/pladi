a6b48203f96a11e2e579e91be0783b5d31093874
Steve Forse <steve.forse@gmail.com>
2026-02-28 05:20:22 +0700

    Extract prioritized_movies helper to reduce AbcSize

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -19,19 +19,14 @@ module Api
 
       render json: {
         sections: serialize_sections(enriched),
-        cached_poster_ids: cached.map { |m| m[:id] },
+        cached_poster_ids: cached.pluck(:id),
         uncached_poster_movies: uncached
       }
     end
 
     def warm_posters
-      priority_ids = Array(params[:priority_ids]).map(&:to_s)
-      all_movies   = Array(params[:movies]).map { |m| m.permit(:id, :thumb).to_h }
-
-      prioritized = all_movies.sort_by { |m| priority_ids.include?(m[:id].to_s) ? 0 : 1 }
-
+      prioritized = prioritized_movies
       WarmPostersJob.perform_later(@server.id, prioritized) if prioritized.any?
-
       head :ok
     end
 
@@ -50,10 +45,16 @@ module Api
       sections.map { |s| s.slice(:title, :movies) }
     end
 
+    def prioritized_movies
+      priority_ids = Array(params[:priority_ids]).map(&:to_s)
+      movies = Array(params[:movies]).map { |m| m.permit(:id, :thumb).to_h }
+      movies.sort_by { |m| priority_ids.include?(m[:id].to_s) ? 0 : 1 }
+    end
+
     def collect_poster_movies(enriched)
       all_movies    = enriched.flat_map { |s| s[:movies] }.uniq { |m| m[:id] }
       poster_movies = all_movies.filter_map { |m| { id: m[:id], thumb: m[:thumb] } if m[:thumb] }
-      cached_ids    = service.posters_cached(poster_movies.map { |m| m[:id] })
+      cached_ids    = service.posters_cached(poster_movies.pluck(:id))
       poster_movies.partition { |m| cached_ids.include?(m[:id]) }
     end
 
